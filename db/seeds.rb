1afe018ed71eebd186409655968515fed5550959
Filipe Ribeiro <filiperibeirom7@gmail.com>
2016-06-02 18:59:47 -0300

    Creating default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(id:1, name_category:"Eletrônicos")
+Category.create(id:2, name_category:"Cosméticos")
+Category.create(id:3, name_category:"Brinquedos")
+Category.create(id:4, name_category:"Roupas")
+Category.create(id:5, name_category:"Remédios")
+Category.create(id:6, name_category:"Suplementos")
