5aa3b99759f399df6417bd55df6e135f53692ad6
Ruan Donato <ruandonato@outlook.com>
2016-06-23 19:19:23 -0300

    passing chat get instance for model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,7 +14,7 @@ class MessagesController < ApplicationController
     @message.receiver_id = @@receiver_id
     @message.chat_id = @@chat_id
 
-    get_istance_chat(@message)
+    Chat.get_istance(@message, @@advertisement_id , @@receiver_id, @@chat_id, current_user)
 
       if @message.save
         redirect_to "/chats/#{@message.chat_id}"
@@ -23,19 +23,6 @@ class MessagesController < ApplicationController
       end
   end
 
-
-  def get_istance_chat(message)
-    if message.chat_id == nil
-      chat = Chat.new
-      chat.advertisement_id = @@advertisement_id
-      chat.receiver_id = @@receiver_id
-      chat.sender_id = current_user.id
-      chat.save
-      message.chat_id = chat.id
-    else
-      message.chat_id = @@chat_id
-    end
-  end
 
   def list_messages
     @list = Message.all
